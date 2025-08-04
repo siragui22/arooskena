129a5ab2404478c8bb27d309c680e8df02909c10
siragui22 <siragui22@gmail.com>
2025-08-04 04:10:03 +0300

    first commit

---
+++ app/header/page.jsx
@@ -0,0 +1,22 @@
+import logo from "../../public/logo.svg";
+import Image from "next/image";
+
+const header = () => {
+  return (
+    <div>
+        <div className="navbar bg-base-100 shadow-sm">
+  <div className="flex-1">
+        <Image
+      src="/logo.svg"
+      alt="Picture of the author"
+      width={100}
+      height={100}
+    />
+  </div>
+
+</div>
+    </div>
+  )
+}
+
+export default header
---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import header from "./header/page";
+
+export default function Home() {
+  return (
+    <div className="">
+
+  
+      <header />
+
+
+    </div>
+  );
+}
