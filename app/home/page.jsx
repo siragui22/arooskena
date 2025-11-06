9114050b1a2c3d0394ef321c312cb0063312c0b7
siragui22 <siragui22@gmail.com>
2025-11-07 01:04:43 +0300

    fix: corriger chemin import Navbar dans app/home/page.jsx pour Vercel

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Carousel from '@/components/Carousel';
-import Navbar from "../components/Navbar";
+import Navbar from '@/components/Navbar';
 
 const Home = () => {
   return (
